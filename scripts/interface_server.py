0cd19a0403b751a6ebdf745abc1c21c1f494b9bb
Ferdian Jovan <fxj345@cs.bham.ac.uk>
2015-02-10 23:51:59 +0000

    Added guide interface server

---
+++ scripts/interface_server.py
@@ -0,0 +1,94 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+import rospy
+import roslib
+import actionlib
+
+import strands_webserver.page_utils as page_utils
+import strands_webserver.client_utils as client_utils
+from strands_webserver.srv import CallButton, CallButtonResponse
+from aaf_walking_group.msg import InterfaceAction, InterfaceResult
+
+
+class InterfaceServer(object):
+
+    def __init__(self, name):
+        # Variables
+        self.request_name = ''
+        self._action_name = name
+        self.display_no = rospy.get_param("~display", 0)
+
+        # tell the webserver where it should look for web files to serve
+        http_root = os.path.join(
+            roslib.packages.get_pkg_dir("aaf_walking_group"),
+            "www")
+        client_utils.set_http_root(http_root)
+
+        # Starting server
+        rospy.loginfo("%s: Starting action server", name)
+        self._as = actionlib.SimpleActionServer(
+            self._action_name,
+            InterfaceAction,
+            execute_cb=self.executeCallback,
+            auto_start=False
+        )
+        self._as.register_preempt_callback(self.preemptCallback)
+        self._as.start()
+        rospy.loginfo("%s: ...done.", name)
+
+        # Services for buttons
+        rospy.Service(name+'/button', CallButton, self.button)
+
+    def executeCallback(self, goal):
+        self.request_name = ''
+        result = InterfaceResult()
+        client_utils.display_relative_page(self.display_no, 'guiding.html')
+
+        while self.request_name == '':
+            rospy.sleep(0.1)
+
+        if self.request_name == 'next':
+            result.chosen_point = goal.next_point
+            rospy.loginfo(result)
+            # client_utils.display_relative_page(self.display_no,
+            #                                    'next_waypoint.html')
+            self._as.set_succeeded(result)
+        elif self.request_name == 'abort':
+            self.preemptCallback()
+            # client_utils.display_relative_page(self.display_no, 'abort.html')
+        else:
+            self.request_name = ''
+            self.listWaypointPage(goal.possible_points)
+            while self.request_name == '':
+                rospy.sleep(0.1)
+            result.chosen_point = self.request_name
+            rospy.loginfo(result)
+            # client_utils.display_relative_page(self.display_no,
+            #                                    'next_waypoint.html')
+            self._as.set_succeeded(result)
+
+    def listWaypointPage(self, possible_points):
+        notice = 'Choose your waypoints.'
+        buttons = []
+        for i in range(len(possible_points)):
+            buttons.append((possible_points[i], 'button'))
+        content = page_utils.generate_named_button_page(notice, buttons,
+                                                        self._action_name)
+        client_utils.display_content(self.display_no, content)
+
+    def preemptCallback(self):
+        rospy.logwarn("Aborting the goal...")
+        self._as.set_preempted()
+
+    def button(self, request):
+        self.request_name = request.name
+        return CallButtonResponse()
+
+
+if __name__ == '__main__':
+    rospy.init_node('interface_server')
+    InterfaceServer(rospy.get_name())
+    rospy.spin()
