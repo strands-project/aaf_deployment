6e7eed374f6064ea3be942980047a09764037a63
Bruno Lacerda <b.lacerda@cs.bham.ac.uk>
2015-02-10 10:33:14 +0100

    skeleton for person approacher

---
+++ approach_person_of_interest/scripts/person_approacher.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+import rospy
+from actionlib import SimpleActionServer, SimpleActionClient
+
+
+
+    
+class PersonApproacher(object):
+
+    def __init__(self:
+        
+        #put stuff
+
+
+    def main(self):
+        # Wait for control-c
+        rospy.spin()
+
+
+
+if __name__ == '__main__':
+    rospy.init_node('person_approacher')
+    person_app = PersonApproacher()
+    person_app.main()
+    
+    
