53616d07a2ec539c4429d560e106746e1a8657cd
Bruno Lacerda <b.lacerda@cs.bham.ac.uk>
2015-02-10 16:56:25 +0100

    stuff

--- approach_person_of_interest/scripts/person_approacher.py
+++ approach_person_of_interest/scripts/person_approacher.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python
 
 import rospy
+import tf
 from std_msgs.msg import String
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, PointStamped
 
 from actionlib import SimpleActionServer, SimpleActionClient
 from approach_person_of_interest.srv import Activate 
@@ -16,6 +17,7 @@ class PersonApproacher(object):
         self.activate=rospy.Service('info_terminal/activate_approacher',Activate,self.activate_cb)
         self.card_seer=rospy.Subscriber("/socialCardReader/commands", String, self.card_callback)
         self.card_pose_getter=rospy.Subscriber("/socialCardReader/cardposition", PoseStamped, self.card_pose_callback)
+        self.tf_ros = tf.TransformerROS()
 
     def activate_cb(self,req): 
         self.active=req.activate
@@ -25,7 +27,13 @@ class PersonApproacher(object):
         rospy.loginfo(rospy.get_caller_id() + "I see a  %s", data.data)
     
     def card_pose_callback(self, data):
-        rospy.loginfo(rospy.get_caller_id() + "The Card pose is %s", data.data)
+        rospy.loginfo(rospy.get_caller_id() + "The Card pose is %s", data.pose)
+        xtion_frame_pose=PointStamped(point=data.pose.position)
+        xtion_frame_pose.header.frame_id='/head_xtion_rgb_optical_frame'
+        map_frame_pose=self.tf_ros.transformPoint('base_link', xtion_frame_pose)
+        
+        print "TESTE", map_frame_pose
+        
 
     def main(self):
         # Wait for control-c
