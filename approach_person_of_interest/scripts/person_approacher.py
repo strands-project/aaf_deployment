5de7be85d64e71924eafafc1cece19ba84b160d8
Bruno Lacerda <b.lacerda@cs.bham.ac.uk>
2015-02-10 11:08:10 +0100

    added activation service

--- approach_person_of_interest/scripts/person_approacher.py
+++ approach_person_of_interest/scripts/person_approacher.py
@@ -2,16 +2,22 @@
 
 import rospy
 from actionlib import SimpleActionServer, SimpleActionClient
-
+from approach_person_of_interest.srv import Activate 
 
 
     
 class PersonApproacher(object):
 
-    def __init__(self:
-        
+    def __init__(self):
+        self.active=False
+        self.activate=rospy.Service('info_terminal/activate_approacher',Activate,self.activate_cb)
+
         #put stuff
 
+    def activate_cb(self,req): 
+        self.active=req.activate
+        return True
+    
 
     def main(self):
         # Wait for control-c
@@ -23,5 +29,3 @@ if __name__ == '__main__':
     rospy.init_node('person_approacher')
     person_app = PersonApproacher()
     person_app.main()
-    
-    
