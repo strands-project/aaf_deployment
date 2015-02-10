5d81a7c4f56bd3ce0cb04541eb774b2cebe6929d
Akshaya <akshayats@gmail.com>
2015-02-10 12:46:53 +0100

    Included subscriber to social_card_reader from TomK

--- approach_person_of_interest/scripts/person_approacher.py
+++ approach_person_of_interest/scripts/person_approacher.py
@@ -11,20 +11,21 @@ class PersonApproacher(object):
     def __init__(self):
         self.active=False
         self.activate=rospy.Service('info_terminal/activate_approacher',Activate,self.activate_cb)
-
+        self.card_seer=rospy.Subscriber("/socialCardReader/commands", String, self.card_callback)
         #put stuff
 
     def activate_cb(self,req): 
         self.active=req.activate
         return True
+
+    def card_callback(self, data):
+        rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
     
 
     def main(self):
         # Wait for control-c
         rospy.spin()
-
-
-
+    
 if __name__ == '__main__':
     rospy.init_node('person_approacher')
     person_app = PersonApproacher()
