b6d86b0477723ba18e50ac8876bd38e659eb7be1
Thomas Fäulhammer <faeulhammer@acin.tuwien.ac.at>
2015-02-11 10:27:18 +0100

    -

--- approach_person_of_interest/scripts/goToPersonServer.py
+++ approach_person_of_interest/scripts/goToPersonServer.py
@@ -3,19 +3,21 @@
 import roslib
 import rospy
 import actionlib
-import approach_person_of_interest
+#import approach_person_of_interest
 from std_msgs.msg import String, Float32, Bool
 from geometry_msgs.msg import PoseStamped, PointStamped
+from approach_person_of_interest.msg import *
 
 class goToPersonAction(object):
-  _feedback = approach_person_of_interest.msg.goToPersonFeedback()
-  _result   = approach_person_of_interest.msg.goToPersonResult()
+  _feedback = goToPersonFeedback()
+  _result   = goToPersonResult()
 
   def __init__(self, name):
 #! /usr/bin/env python    
     self._action_name = name
     self._as = actionlib.SimpleActionServer(self._action_name, approach_person_of_interest.msg.goToPersonAction, execute_cb=self.execute_cb, auto_start = False)
     self._as.start()
+    print 'go_to_person action server started.'
     
   def execute_cb(self, goal):
     # helper variables
