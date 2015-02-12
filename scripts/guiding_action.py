4f7c64f391e95badcfc671262c0d50617ac4cdf6
Joao <j.pedrosntos@gmail.com>
2015-02-12 09:04:56 +0000

    .

--- scripts/guiding_action.py
+++ scripts/guiding_action.py
@@ -3,6 +3,7 @@ import rospy
 import actionlib
 #import tf
 import numpy
+import mutex
 
 from aaf_walking_group.msg import GuidingAction
 from aaf_walking_group.msg import EmptyAction, EmptyActionGoal
@@ -21,15 +22,15 @@ class GuidingServer():
         self.empty_client = actionlib.SimpleActionClient('wait_for_participant', EmptyAction)
         self.empty_client.wait_for_server()
         self.card_subscriber =rospy.Subscriber("/socialCardReader/QSR_generator", String, self.card_callback)
-        self.odom_subscriber =rospy.Subscriber("odom", Odometry, self.odom_callback)
+        self.odom_subscriber =rospy.Subscriber("odom", Odometry, self.odom_callback, queue_size=1 )
         self.last_location = Odometry()
         self.pause = 0;
-        self.begin = 0;
+        self.begin = 1;
+        self.counter = 0;
       
     def execute(self, goal):
       
         self.begin = 1;
-         
         navgoal = topological_navigation.msg.GotoNodeGoal()
         navgoal.target = goal.waypoint
         self.client.send_goal(navgoal)
@@ -43,37 +44,42 @@ class GuidingServer():
         self.client.cancel_all_goals()
       
     def card_callback(self, data):
+        self.odom_subscriber.unregister()
         if self.pause == 1:
             #call action server
             if data.data == 'near':
                 self.empty_client.send_goal_and_wait(EmptyActionGoal())
-            try:
-                pause_service = rospy.ServiceProxy('/monitored_navigation/pause_nav', PauseResumeNav)
-                pause_service(0)
-                self.pause = 0
-            except rospy.ServiceException, e:
-                print "Service call failed: %s" % e
-       
+                try:
+                    pause_service = rospy.ServiceProxy('/monitored_navigation/pause_nav', PauseResumeNav)
+                    pause_service(0)
+                    self.pause = 0
+                except rospy.ServiceException, e:
+                    print "Service call failed: %s" % e
+        self.odom_subscriber =rospy.Subscriber("odom", Odometry, self.odom_callback)
+
 
     def odom_callback(self, data):
         if self.begin == 0:
-            x = data.pose.pose.position.x - self.last_location.pose.pose.position.x
-            y = data.pose.pose.position.y - self.last_location.pose.pose.position.y
-               
-            lenght = numpy.sqrt(x*x + y*y)
-               
-            if lenght >= 2.0:
-                try:
-                    pause_service = rospy.ServiceProxy('/monitored_navigation/pause_nav', PauseResumeNav)
-                    pause_service(1)
-                    self.pause = 1
-                    self.begin = 1
-                except rospy.ServiceException, e:
-                    print "Service call failed: %s" % e
+            if self.counter == 10:
+                x = data.pose.pose.position.x - self.last_location.pose.pose.position.x
+                y = data.pose.pose.position.y - self.last_location.pose.pose.position.y
+                   
+                lenght = numpy.sqrt(x*x + y*y)
+                   
+                if lenght >= 2.0:
+                    try:
+                        pause_service = rospy.ServiceProxy('/monitored_navigation/pause_nav', PauseResumeNav)
+                        pause_service(1)
+                        self.pause = 1
+                        self.begin = 1
+                    except rospy.ServiceException, e:
+                        print "Service call failed: %s" % e
         else:
             self.last_location.pose.pose.position.x = data.pose.pose.position.x;
             self.last_location.pose.pose.position.y = data.pose.pose.position.y;
             self.begin = 0;
+        
+        self.counter += 1
             
                         
        
