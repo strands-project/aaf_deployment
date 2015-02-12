9c0f8e6d3d03eb68cd26ae9ac1af87a696ac084d
Ferdian Jovan <fxj345@cs.bham.ac.uk>
2015-02-12 15:58:42 +0000

    Fixing guiding_action.py that I messed up

--- scripts/guiding_action.py
+++ scripts/guiding_action.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python
 import rospy
 import actionlib
-#import tf
+# import tf
 import numpy
-import mutex
+# import mutex
 
 from aaf_walking_group.msg import GuidingAction, GuidingGoal
 from aaf_walking_group.msg import EmptyAction, EmptyActionGoal
@@ -33,7 +33,6 @@ class GuidingServer():
             EmptyAction
         )
         self.empty_client.wait_for_server()
-<<<<<<< HEAD
         self.card_subscriber = rospy.Subscriber(
             "/socialCardReader/QSR_generator",
             String,
@@ -42,11 +41,13 @@ class GuidingServer():
         self.odom_subscriber = rospy.Subscriber(
             "odom",
             Odometry,
-            self.odom_callback
+            self.odom_callback,
+            queue_size=1
         )
         self.last_location = Odometry()
         self.pause = 0
-        self.begin = 0
+        self.begin = 1
+        self.counter = 0
 
         self.client_walking_interface = actionlib.SimpleActionClient(
             'walking_interface_server',
@@ -56,19 +57,6 @@ class GuidingServer():
 
     def execute(self, goal):
         self.begin = 1
-
-=======
-        self.card_subscriber =rospy.Subscriber("/socialCardReader/QSR_generator", String, self.card_callback)
-        self.odom_subscriber =rospy.Subscriber("odom", Odometry, self.odom_callback, queue_size=1 )
-        self.last_location = Odometry()
-        self.pause = 0;
-        self.begin = 1;
-        self.counter = 0;
-      
-    def execute(self, goal):
-      
-        self.begin = 1;
->>>>>>> 963e31b45d6832cd48835badbe5937bfb4e91984
         navgoal = topological_navigation.msg.GotoNodeGoal()
         navgoal.target = goal.waypoint
         self.client.send_goal(navgoal)
@@ -82,8 +70,8 @@ class GuidingServer():
 
         # self.client.wait_for_result()
         ps = self.client.get_result()
-        self.server.set_succeeded()
         print ps
+        self.server.set_succeeded()
         # what to do when it fails?
 
     def _on_node_shutdown(self):
@@ -93,86 +81,55 @@ class GuidingServer():
         if self.pause == 1:
             # call action server
             if data.data == 'near':
-       		self.odom_subscriber = None
+                self.odom_subscriber = None
                 self.empty_client.send_goal_and_wait(EmptyActionGoal())
-<<<<<<< HEAD
-            try:
-                pause_service = rospy.ServiceProxy(
-                    '/monitored_navigation/pause_nav',
-                    PauseResumeNav
-                )
-                pause_service(0)
-                self.pause = 0
-            except rospy.ServiceException, e:
-                print "Service call failed: %s" % e
-
-    def odom_callback(self, data):
-        if self.begin == 0:
-            x = data.pose.pose.position.x - self.last_location.pose.pose.position.x
-            y = data.pose.pose.position.y - self.last_location.pose.pose.position.y
-
-            lenght = numpy.sqrt(x*x + y*y)
-
-            if lenght >= 2.0:
                 try:
                     pause_service = rospy.ServiceProxy(
                         '/monitored_navigation/pause_nav',
                         PauseResumeNav
                     )
-                    pause_service(1)
-                    self.pause = 1
-                    self.begin = 1
-=======
-                try:
-                    pause_service = rospy.ServiceProxy('/monitored_navigation/pause_nav', PauseResumeNav)
                     pause_service(0)
                     self.pause = 0
                     print "the guy is near, fear him"
->>>>>>> 963e31b45d6832cd48835badbe5937bfb4e91984
                 except rospy.ServiceException, e:
                     print "Service call failed: %s" % e
-        	self.odom_subscriber =rospy.Subscriber("odom", Odometry, self.odom_callback)
-
+                self.odom_subscriber = rospy.Subscriber("odom", Odometry,
+                                                        self.odom_callback)
 
     def odom_callback(self, data):
         if self.begin == 0:
             if self.counter == 10:
-                x = data.pose.pose.position.x - self.last_location.pose.pose.position.x
-                y = data.pose.pose.position.y - self.last_location.pose.pose.position.y
-                   
+                x = data.pose.pose.position.x - \
+                    self.last_location.pose.pose.position.x
+                y = data.pose.pose.position.y - \
+                    self.last_location.pose.pose.position.y
+
                 lenght = numpy.sqrt(x*x + y*y)
-                   
+
                 if lenght >= 2.0:
-                   # self.odom_subscriber.unregister()
+                    # self.odom_subscriber.unregister()
                     print "reached 2.0 meters"
                     try:
-                        pause_service = rospy.ServiceProxy('/monitored_navigation/pause_nav', PauseResumeNav)
+                        pause_service = rospy.ServiceProxy(
+                            '/monitored_navigation/pause_nav',
+                            PauseResumeNav
+                        )
                         pause_service(1)
                         self.pause = 1
                         self.begin = 1
                         print "pause"
                     except rospy.ServiceException, e:
                         print "Service call failed: %s" % e
-                    #self.odom_subscriber =rospy.Subscriber("odom", Odometry, self.odom_callback)
+                    # self.odom_subscriber = rospy.Subscriber("odom", Odometry, self.odom_callback)
                 self.counter = 0
         else:
-<<<<<<< HEAD
             self.last_location.pose.pose.position.x = data.pose.pose.position.x
             self.last_location.pose.pose.position.y = data.pose.pose.position.y
             self.begin = 0
 
-
-=======
-            self.last_location.pose.pose.position.x = data.pose.pose.position.x;
-            self.last_location.pose.pose.position.y = data.pose.pose.position.y;
-            self.begin = 0;
-        
         self.counter += 1
-            
-                        
-       
-       
->>>>>>> 963e31b45d6832cd48835badbe5937bfb4e91984
+
+
 if __name__ == '__main__':
     rospy.init_node('guiding_server')
     server = GuidingServer()
