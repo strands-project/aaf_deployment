b2835f73a96dfcf0a9f1ab3375ce9f376bbd8cd9
Christian Dondrup <cdondrup@lincoln.ac.uk>
2015-03-22 21:58:57 +0000

    Guiding state actually works now. Finishing with success when last waypoint is reached, showing the key card will bring you to the guide interface where with "Weiter" you just resume to the waypoint.

--- aaf_walking_group/src/aaf_walking_group/guiding.py
+++ aaf_walking_group/src/aaf_walking_group/guiding.py
@@ -3,19 +3,26 @@
 import rospy
 import smach
 from copy import deepcopy
+from std_msgs.msg import String
 import actionlib
+from actionlib_msgs.msg import GoalStatus
 from aaf_walking_group.msg import GuidingAction, GuidingGoal
 from music_player.srv import MusicPlayerService, MusicPlayerServiceRequest
 
 
 class Guiding(smach.State):
-    def __init__(self):
+    def __init__(self, waypoints):
         smach.State.__init__(
             self,
             outcomes=['reached_point', 'reached_final_point', 'key_card', 'killall'],
             input_keys=['waypoint'],
             output_keys=['current_waypoint']
         )
+        self.waypoints = waypoints
+        self.last_waypoint = waypoints[str(max([int(x) for x in waypoints.keys()]))]
+        self.previous_waypoint = waypoints[str(min([int(x) for x in waypoints.keys()]))]
+        self.sub = None
+        self.card = False
         rospy.loginfo("Creating guiding client...")
         self.nav_client = actionlib.SimpleActionClient("guiding", GuidingAction)
         self.nav_client.wait_for_server()
@@ -34,33 +41,47 @@ class Guiding(smach.State):
     def execute(self, userdata):
         rospy.loginfo("Guiding group")
         rospy.sleep(1)
+        self.card = False
+
         rospy.loginfo("I am going to: " + userdata.waypoint)
         # Action server that does all the black magic for navigation
+        for elem in self.waypoints.items():
+            if elem[1] == userdata.waypoint:
+                key = str(int(elem[0])-1)
+                if not key in self.waypoints.keys():
+                    key = "1"
+                self.previous_waypoint = self.waypoints[key]
 
         self.music_control("play")
 
         goal = GuidingGoal()
-        print goal
         goal.waypoint = userdata.waypoint
-        self.nav_client.send_goal_and_wait(goal)
+        if not self.card:
+            self.nav_client.send_goal(goal)
+            # Necessary to account for seeing the card immediately after the goal was sent.
+            # That leads to the goal being pending while trying to cancel and thefore not
+            # cancelling, loosing every chance of doing so. Solution: wait until not pending
+            # before subscribing to card reader for preempt callback.
+            while self.nav_client.get_state() == GoalStatus.PENDING:
+                rospy.sleep(0.01)
+            self.sub = rospy.Subscriber("/socialCardReader/commands", String, callback=self.callback)
+            self.nav_client.wait_for_result()
+            state = self.nav_client.get_state()
 
         self.music_control("pause")
 
-        if self.preempt_requested():
+        if self.preempt_requested() and not self.card:
             return 'killall'
-        else:
-
-            # If successful and not last point
-            userdata.current_waypoint = deepcopy(userdata.waypoint)
-            return 'reached_point'
-
-            # If successful and last point
-            userdata.current_waypoint = deepcopy(userdata.waypoint)
-            return 'reached_final_point'
-
-            # If aborted by showing key card
-            userdata.current_waypoint = deepcopy(userdata.waypoint)
+        elif self.preempt_requested() and self.card:
+            userdata.current_waypoint = self.previous_waypoint
             return 'key_card'
+        else:
+            if state == GoalStatus.SUCCEEDED and not goal.waypoint == self.last_waypoint:
+                userdata.current_waypoint = deepcopy(userdata.waypoint)
+                return 'reached_point'
+            elif state == GoalStatus.SUCCEEDED and goal.waypoint == self.last_waypoint:
+                userdata.current_waypoint = deepcopy(userdata.waypoint)
+                return 'reached_final_point'
 
     def request_preempt(self):
         """Overload the preempt request method to cancel guiding."""
@@ -68,3 +89,11 @@ class Guiding(smach.State):
         self.nav_client.cancel_all_goals()
         smach.State.request_preempt(self)
         rospy.logwarn("Guiding Preempted!")
+
+    def callback(self, data):
+        rospy.loginfo("got card: " + str(data.data))
+        if data.data == "PAUSE_WALK":
+            self.card = True
+            self.sub.unregister()
+            self.sub = None
+            self.request_preempt()
