5eb0688d114119ec81d105bfaf297d2bd4dc8080
Christian Dondrup <cdondrup@lincoln.ac.uk>
2015-04-07 19:41:04 +0100

    Running with 3D obstacle avoidance

--- aaf_bringup/scripts/aaf_start.sh
+++ aaf_bringup/scripts/aaf_start.sh
@@ -41,7 +41,7 @@ tmux select-window -t $SESSION:4
 tmux send-keys "DISPLAY=:0 roslaunch strands_bringup strands_ui.launch"
 
 tmux select-window -t $SESSION:5
-tmux send-keys "DISPLAY=:0 roslaunch strands_bringup strands_navigation.launch map:=/opt/strands/maps/WW_GF_2015_02_22-cropped.yaml with_no_go_map:=false topological_map:=WW_GF_2015_02_22 no_go_map:=/opt/strands/maps/WW_GF_2015_02_22-cropped.yaml with_human_aware:=true"
+tmux send-keys "DISPLAY=:0 roslaunch strands_bringup strands_navigation.launch map:=/opt/strands/maps/WW_GF_2015_02_22-cropped.yaml with_no_go_map:=false topological_map:=WW_GF_2015_02_22 no_go_map:=/opt/strands/maps/WW_GF_2015_02_22-cropped.yaml with_human_aware:=true with_chest_xtion:=true"
 
 tmux select-window -t $SESSION:6
 tmux send-keys "DISPLAY=:0 roslaunch perception_people_launch people_tracker_robot.launch machine:=left-cortex user:=strands"
