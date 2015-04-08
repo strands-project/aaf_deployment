5afdb76d7970d5636ce22c27bb5e61acf3315e02
Jaime Pulido Fentanes <pulidofentanes@gmail.com>
2015-04-08 05:05:43 +0100

    Infoterminal waypoint proposer.

--- info_terminal/infremen/src/CFrelementSet.cpp
+++ info_terminal/infremen/src/CFrelementSet.cpp
@@ -14,10 +14,12 @@ CFrelementSet::~CFrelementSet()
 	for (int i=0;i<numFrelements;i++) delete frelements[i];
 }
 
-int CFrelementSet::add(const char *name,uint32_t times[],unsigned char states[],int length)
+int CFrelementSet::add(const char *name,uint32_t times[],unsigned char states[],int length,bool createNew)
 {
-	bool exists = find(name);
-	if (exists == false){
+	int exists = find(name);
+	if (exists < 0 && createNew == false) return -1;
+	if (exists < 0 && createNew == true )
+	{
 		frelements[numFrelements++] = new CFrelement(name);
 		activeIndex = numFrelements-1;
 		active = frelements[numFrelements-1];
@@ -28,37 +30,37 @@ int CFrelementSet::add(const char *name,uint32_t times[],unsigned char states[],
 
 int CFrelementSet::evaluate(const char *name,uint32_t times[],unsigned char states[],int length,int order,float errors[])
 {
-	if (find(name) == false) return -1;
+	if (find(name) < 0) return -1;
 	return active->evaluate(times,states,length,order,errors);;
 }
 
 int CFrelementSet::estimate(const char *name,uint32_t times[],float probs[],int length,int order)
 {
-	if (find(name) == false)return -1;
+	if (find(name) < 0) return -1;
 	//printf("Estimate %i %s \n",activeIndex,active->id);
 	return active->estimate(times,probs,length,order);
 }
 
 int CFrelementSet::estimateEntropy(const char *name,uint32_t times[],float entropy[],int length,int order)
 {
-	if (find(name) == false)return -1;
+	if (find(name) < 0) return -1;
 	//printf("Estimate %i %s \n",activeIndex,active->id);
 	return active->estimateEntropy(times,entropy,length,order);
 }
 
-bool CFrelementSet::find(const char *name)
+int CFrelementSet::find(const char *name)
 {
 	int i = 0;
 	for (i =0;(i<numFrelements) && (strcmp(frelements[i]->id,name)!=0);i++) {}
-	if (i==numFrelements) return false;
+	if (i==numFrelements) return -1;
 	activeIndex = i;	
 	active = frelements[i];
-	return true;
+	return activeIndex;
 }
 
 int CFrelementSet::remove(const char *name)
 {
-	if (find(name) == false) return -numFrelements;
+	if (find(name) < 0) return -numFrelements;
 	delete frelements[activeIndex];
 	frelements[activeIndex] = frelements[--numFrelements];
 	return numFrelements+1;
@@ -66,7 +68,7 @@ int CFrelementSet::remove(const char *name)
 
 bool CFrelementSet::update(const char* name,int order)
 {
-	if (find(name) == false) return false;
+	if (find(name) < 0) return false;
 	active->update(order);
 	return true;
 }
