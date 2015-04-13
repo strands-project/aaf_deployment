4573fc64db6a04734b3512d8c1a81efcfc4e7530
Christian Dondrup <cdondrup@lincoln.ac.uk>
2015-04-13 17:32:05 +0200

    Fixing start-up bugs

--- aaf_bringup/scripts/aaf_start.sh
+++ aaf_bringup/scripts/aaf_start.sh
@@ -13,11 +13,11 @@ tmux new-window -t $SESSION:5 -n 'navigation'
 tmux new-window -t $SESSION:6 -n 'ppl_perception'
 tmux new-window -t $SESSION:7 -n 'media_server'
 tmux new-window -t $SESSION:8 -n 'axlaunch_server'
-tmux new-window -t $SESSION:9 -n 'scheduler'
-tmux new-window -t $SESSION:10 -n 'logging'
-tmux new-window -t $SESSION:11 -n 'info_terminal'
-tmux new-window -t $SESSION:12 -n 'bell_bot'
-tmux new-window -t $SESSION:13 -n 'walking_group'
+tmux new-window -t $SESSION:9 -n 'logging'
+tmux new-window -t $SESSION:10 -n 'info_terminal'
+tmux new-window -t $SESSION:11 -n 'bell_bot'
+tmux new-window -t $SESSION:12 -n 'walking_group'
+tmux new-window -t $SESSION:13 -n 'scheduler'
 
 
 tmux select-window -t $SESSION:0
@@ -35,7 +35,7 @@ tmux select-window -t $SESSION:2
 tmux send-keys "DISPLAY=:0 roslaunch strands_bringup strands_robot.launch with_mux:=false"
 
 tmux select-window -t $SESSION:3
-tmux send-keys "DISPLAY=:0 roslaunch strands_bringup strands_cameras.launch head_camera:=true head_ip:=left-cortex head_user:=strands chest_camera:=true chest_ip:=right-cortex chest_user:=strands"
+tmux send-keys "DISPLAY=:0 roslaunch strands_bringup strands_cameras.launch head_camera:=true head_ip:=werner-left-cortex head_user:=strands chest_camera:=true chest_ip:=werner-right-cortex chest_user:=strands"
 
 tmux select-window -t $SESSION:4
 tmux send-keys "HOST_IP=192.168.0.100 DISPLAY=:0 roslaunch strands_bringup strands_ui.launch mary_machine:=werner-right-cortex mary_machine_user:=strands"
@@ -53,19 +53,19 @@ tmux select-window -t $SESSION:8
 tmux send-keys "DISPLAY=:0 rosrun roslaunch_axserver roslaunch_server.py"
 
 tmux select-window -t $SESSION:9
-tmux send-keys "DISPLAY=:0 roslaunch aaf_bringup aaf_routine.launch calendar:=henry.strands%40hanheide.net machine:=werner-right-cortex user:=strands"
-
-tmux select-window -t $SESSION:10
 tmux send-keys "DISPLAY=:0 roslaunch aaf_logging logging.launch"
 
-tmux select-window -t $SESSION:11
+tmux select-window -t $SESSION:10
 tmux send-keys "DISPLAY=:0  roslaunch info_terminal info_terminal.launch"
 
-tmux select-window -t $SESSION:12
+tmux select-window -t $SESSION:11
 tmux send-keys "DISPLAY=:0 TODO: launch this with axlaunch_server roslaunch aaf_bringup aaf_bellbot.launch"
 
-tmux select-window -t $SESSION:13
+tmux select-window -t $SESSION:12
 tmux send-keys "DISPLAY=:0 roslaunch aaf_walking_group task_servers.launch"
+
+tmux select-window -t $SESSION:13
+tmux send-keys "DISPLAY=:0 roslaunch aaf_bringup aaf_routine.launch calendar:=henry.strands%40hanheide.net machine:=werner-right-cortex user:=strands topological_map:=aaf_predep"
 
 # Set default window
 tmux select-window -t $SESSION:0
