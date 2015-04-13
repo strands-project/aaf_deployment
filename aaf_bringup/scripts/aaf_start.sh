b0116119d53be5e5b27704a07d2620b49a6cc530
Christian Dondrup <cdondrup@lincoln.ac.uk>
2015-04-13 16:53:27 +0200

    Changing parameters to run on Werner

--- aaf_bringup/scripts/aaf_start.sh
+++ aaf_bringup/scripts/aaf_start.sh
@@ -29,22 +29,22 @@ tmux select-pane -t 1
 tmux send-keys "htop" C-m
 
 tmux select-window -t $SESSION:1
-tmux send-keys "DISPLAY=:0 roslaunch strands_bringup strands_core.launch db_path:=/opt/strands/aaf_datacentre"
+tmux send-keys "DISPLAY=:0 roslaunch strands_bringup strands_core.launch"
 
 tmux select-window -t $SESSION:2
-tmux send-keys "DISPLAY=:0 roslaunch strands_bringup strands_robot.launch laser:=/dev/laser with_mux:=false"
+tmux send-keys "DISPLAY=:0 roslaunch strands_bringup strands_robot.launch with_mux:=false"
 
 tmux select-window -t $SESSION:3
 tmux send-keys "DISPLAY=:0 roslaunch strands_bringup strands_cameras.launch head_camera:=true head_ip:=left-cortex head_user:=strands chest_camera:=true chest_ip:=right-cortex chest_user:=strands"
 
 tmux select-window -t $SESSION:4
-tmux send-keys "DISPLAY=:0 roslaunch strands_bringup strands_ui.launch mary_machine:=right-cortex mary_machine_user:=strands"
+tmux send-keys "HOST_IP=192.168.0.100 DISPLAY=:0 roslaunch strands_bringup strands_ui.launch mary_machine:=werner-right-cortex mary_machine_user:=strands"
 
 tmux select-window -t $SESSION:5
-tmux send-keys "DISPLAY=:0 roslaunch aaf_bringup aaf_navigation.launch map:=/opt/strands/maps/WW_GF_2015_02_22-cropped.yaml topological_map:=WW_GF_2015_02_22"
+tmux send-keys "DISPLAY=:0 roslaunch aaf_bringup aaf_navigation.launch map:=/opt/strands/map/aaf_winter.yaml topological_map:=aaf_predep"
 
 tmux select-window -t $SESSION:6
-tmux send-keys "DISPLAY=:0 roslaunch perception_people_launch people_tracker_robot.launch machine:=left-cortex user:=strands"
+tmux send-keys "DISPLAY=:0 roslaunch perception_people_launch people_tracker_robot.launch machine:=werner-left-cortex user:=strands"
 
 tmux select-window -t $SESSION:7
 tmux send-keys "DISPLAY=:0 rosrun mongodb_media_server server.py"
@@ -53,7 +53,7 @@ tmux select-window -t $SESSION:8
 tmux send-keys "DISPLAY=:0 rosrun roslaunch_axserver roslaunch_server.py"
 
 tmux select-window -t $SESSION:9
-tmux send-keys "DISPLAY=:0 roslaunch aaf_bringup aaf_routine.launch calendar:=henry.strands%40hanheide.net machine:=right-cortex user:=strands"
+tmux send-keys "DISPLAY=:0 roslaunch aaf_bringup aaf_routine.launch calendar:=henry.strands%40hanheide.net machine:=werner-right-cortex user:=strands"
 
 tmux select-window -t $SESSION:10
 tmux send-keys "DISPLAY=:0 roslaunch aaf_logging logging.launch"
