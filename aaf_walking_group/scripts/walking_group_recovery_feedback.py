3d20cb0868eccef91e949217b55ab589b6320027
Christian Dondrup <cdondrup@lincoln.ac.uk>
2016-03-20 14:42:55 +0000

    Adding recovery feedback node.

---
+++ aaf_walking_group/scripts/walking_group_recovery_feedback.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import rospy
+from nav_msgs.msg import Path
+from actionlib_msgs.msg import GoalStatusArray
+from strands_webserver.msg import ModalDlg
+
+
+class Dialogue():
+    def __init__(self):
+        self.__visible = False
+        self.pub = rospy.Publisher("/strands_webserver/modal_dialog", ModalDlg, queue_size=1)
+
+        self.m = ModalDlg()
+        self.m.title = "Ich stecke fest!"
+        self.m.content = "<b>Ich versuche gerade mich zu befreien. Bitte habe etwas Geduld.</b>"
+
+    def __call__(self, show):
+        if show is not self.__visible:
+            self.m.show = show
+            self.pub.publish(self.m)
+            self.__visible = show
+
+
+class RecoveryFeedback(object):
+    def __init__(self, name):
+        rospy.loginfo("Starting %s ..." % name)
+        self.path_stamp = rospy.Time.now().secs
+        self.dialogue = Dialogue()
+        rospy.Subscriber("/move_base/DWAPlannerROS/local_plan", Path, self.path_cb)
+        rospy.Subscriber("/move_base/status", GoalStatusArray, self.state_cb)
+        rospy.loginfo("... done")
+
+    def path_cb(self, msg):
+        self.path_stamp = msg.header.stamp.secs
+
+    def state_cb(self, msg):
+        if msg.status_list[-1].status == 1:
+            dif = abs(msg.header.stamp.secs - self.path_stamp)
+            if dif > 5.:
+                rospy.logwarn("Robot appears to be stuck.")
+                self.dialogue(True)
+            else:
+                self.dialogue(False)
+        else:
+            self.dialogue(False)
+
+
+if __name__ == "__main__":
+    rospy.init_node("walking_group_recovery_feedback")
+    RecoveryFeedback(rospy.get_name())
+    rospy.spin()
+
