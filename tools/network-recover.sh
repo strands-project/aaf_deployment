609d697512fdc2dc033446f80035c21b0b4e69ca
Marc Hanheide <marc@hanheide.net>
2016-06-08 13:26:40 +0100

    Update network-recover.sh

--- tools/network-recover.sh
+++ tools/network-recover.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-if ! /usr/bin/nmcli c status id "STRANDS AAF" > /dev/null; then
+# make sure you configure /usr/bin/nmcli with the NOPASSWD option in sudoers:
+#   ALL ALL=(ALL) NOPASSWD: /usr/bin/nmcli
+
+ESSID="STRANDS AAF"
+
+# check for connection status, option 1: ask nmcli
+# if ! /usr/bin/nmcli c status id "$ESSID" > /dev/null; then
+
+# check for connection status, option 2: check real connection
+curl -D /tmp/headers-$USER.txt -s 'http://captive.apple.com/hotspot-detect' > /tmp/captive-return-$USER.html
+http_code=`grep "HTTP/1.1" /tmp/headers-$USER.txt | cut -f2 -d" "`
+if [ "$http_code" = "200" ]; then
+    sleep 1
+else
 	echo "connection dropped and needs to be re-initiated"
 	sudo /usr/bin/nmcli nm wifi off
 	sleep 5
         sudo /usr/bin/nmcli nm wifi on
 	sleep 5
-	sudo /usr/bin/nmcli c up id "STRANDS AAF"
- 	#nmcli d wifi connect "STRANDS" password 12345678
+	sudo /usr/bin/nmcli c up id "$ESSID"
 fi
+
