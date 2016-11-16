00994234dd36bf9d672358db03ed69170a9f66f1
Tom Krajnik <tkrajnik@lincoln.ac.uk>
2016-11-16 17:57:28 +0100

    Retrieving node name from frelementSet for planning - test 2.

--- info_terminal/infremen/src/infremen.cpp
+++ info_terminal/infremen/src/infremen.cpp
@@ -481,7 +481,7 @@ int createTask(int slot)
 			if (path.path.size() > 0){
 				for (int i =0;i<path.path.size();i++) ROS_INFO("Path %i: %s",i,graph.nodes[path.path[i]].name.c_str());
 			       	nodes[slot] = frelementSet.find(graph.nodes[path.path[0]].name.c_str());
-				ROS_WARN("Goto %i.",nodes[slot]);
+				ROS_WARN("Goto %i %s.",nodes[slot],frelementSet.frelements[nodes[slot]]->id);
 			}else{
 				ROS_WARN("No path generated! Going to charging station.");
 				nodes[slot]=chargeNodeID;
